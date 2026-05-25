63e4240bb46292f48cb67011c3c8abe8c2f49ecf
GustavoGines <ginesparker95@gmail.com>
2026-05-25 03:51:52 -0300

    fix(android): revert removal of applicationIdSuffix = '.dev'

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,6 +48,7 @@ android {
     productFlavors {
         create("dev") {
             dimension = "default"
+            applicationIdSuffix = ".dev"
             resValue("string", "app_name", "Pastelería 180 (Dev)")
             matchingFallbacks += listOf("staging")
         }
